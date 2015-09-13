b1d5baafaaf7dae353c0678d3029fc8a0a9e8c03
jasonComing <jason.ching@comingtech.com.hk>
2015-09-13 13:53:45 +0800

    Seperate SELECT into lines

--- reports/StuckInIqcFor2Days.sql
+++ reports/StuckInIqcFor2Days.sql
@@ -1,6 +1,15 @@
 -- 到貨2天未入庫報表
-SELECT dep.cDepName AS [部门], m.dDate AS [到货日期], getdate() AS [今天], datediff(d, getdate(), m.dDate) as [天数], 
-d.csocode AS [JOB], m.cCode AS [采购到货单号], d.cInvCode AS [存货编码], i.cInvName AS [存货名称], d.iQuantity AS [数量],d.fValidInQuan AS [合格品入库数量]
+SELECT
+  dep.cDepName AS [部门], 
+  m.dDate AS [到货日期],
+  getdate() AS [今天],
+  datediff(d, getdate(), m.dDate) as [天数], 
+  d.csocode AS [JOB],
+  m.cCode AS [采购到货单号],
+  d.cInvCode AS [存货编码],
+  i.cInvName AS [存货名称],
+  d.iQuantity AS [数量],
+  d.fValidInQuan AS [合格品入库数量]
 FROM PU_ArrivalVouch m  
 JOIN PU_ArrivalVouchs d ON m.ID = d.ID  
 join Inventory i on i.cInvCode = d.cInvCode
